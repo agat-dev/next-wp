f278011137b98f5b2815fe9f07a4ab8ee04e1ba6
agathe <agathe@agat.dev>
2026-01-03 05:27:09 +0100

    v0.1.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { getOptions } from "@/lib/options";
 import ClientHeroBlock from "@/components/client-hero-block";
-import ClientQuizBlock from "@/components/client-quiz-block";
-import ClientComparatifBlock from "@/components/client-comparatif-block";
-import ClientAboutBlock from "@/components/client-about-block";
-import ClientArticlesBlock from "@/components/client-articles-block";
 
 export default async function HomePage() {
   const options = await getOptions();
